0b34048281ef1b20c9fee1035919a71694728b37
Brody Childs <brody.childs@hqo.co>
2026-03-05 11:27:59 -0500

    Icon is now included in frame

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,21 @@ mod parser;
 mod theme;
 
 use eframe::egui;
+use egui::IconData;
+
+fn load_icon() -> IconData {
+    let bytes = include_bytes!("../icons/icon_256.png");
+    let img = image::load_from_memory(bytes).unwrap().into_rgba8();
+    let (w, h) = img.dimensions();
+    IconData { rgba: img.into_raw(), width: w, height: h }
+}
 
 fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([800.0, 600.0])
-            .with_min_inner_size([400.0, 300.0]),
+            .with_min_inner_size([400.0, 300.0])
+            .with_icon(load_icon()),
         ..Default::default()
     };
     eframe::run_native(
